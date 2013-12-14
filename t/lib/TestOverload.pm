233357bef849f60ea398784f76be6b438c1196e8
Karen Etheridge <ether@cpan.org>
2013-12-13 18:37:05 -0800

    fallback option is necessary to prevent MooseX::Role::WithOverloading from blowing up

--- t/lib/TestOverload.pm
+++ t/lib/TestOverload.pm
@@ -2,7 +2,9 @@ package TestOverload;
 use Moose::Role;
 use MooseX::Role::WithOverloading;
 
-use overload q{""} => sub { };
+use overload
+    q{""} => sub { },
+    fallback => 1;
 
 sub foo { };
 
